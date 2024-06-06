691a89b26ea2466982474d853b5fe79faf88ba28
gsstoykov <georgi.stoykov@limechain.tech>
2024-06-06 18:41:09 +0300

    feat: Added contract deploy script and deploy abi
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

---
+++ contract/deploy/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+
+	c "github.com/gsstoykov/go-ethereum-fetcher/contract"
+)
+
+func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("error loading .env file: %v", err)
+		panic(err)
+	}
+
+	ethurl := os.Getenv("ETH_NODE_URL")
+	client, err := ethclient.Dial(ethurl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pkeystr := os.Getenv("PRIVATE_KEY")
+	privateKey, err := crypto.HexToECDSA(pkeystr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	address, tx, instance, err := c.DeploySimplePersonInfoContract(auth, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(address.Hex())
+	fmt.Println(tx.Hash().Hex())
+
+	_ = instance
+}
